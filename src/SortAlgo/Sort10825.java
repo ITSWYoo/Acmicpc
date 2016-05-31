b02364448fa2e4de4a2bc300bf6034f534ce25f0
ITSWYoo <ITSWyoo@gmail.com>
2016-05-31 19:15:04 +0900

    no message

--- src/SortAlgo/Sort10825.java
+++ src/SortAlgo/Sort10825.java
@@ -1,3 +1,5 @@
+package SortAlgo;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
--- src/SortAlgo/Sort11650.java
+++ src/SortAlgo/Sort11650.java
@@ -1,3 +1,5 @@
+package SortAlgo;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
